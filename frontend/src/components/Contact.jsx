6fa04c2e885332561c7a52db48d1db8a809a1256
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-04-05 21:59:36 +0530

    backend deploy link (cylic link) Added

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://ill-blue-gopher-robe.cyclic.app/api/v1/message/send",
         {
           name,
           email,
